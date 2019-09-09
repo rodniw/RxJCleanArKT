b118039c51ff832feaae091debd896752407e5ce
rodniw <rodion.prosvirnin@bk.ru>
2019-09-09 18:14:36 +0300

    get bookmarked projects use case

---
+++ Domain/src/main/java/dev/rodni/ru/domain/interactor/browse/GetBookmarkedProjects.kt
@@ -0,0 +1,19 @@
+package dev.rodni.ru.domain.interactor.browse
+
+import dev.rodni.ru.domain.executor.PostExecutionThread
+import dev.rodni.ru.domain.interactor.ObservableUseCase
+import dev.rodni.ru.domain.model.Project
+import dev.rodni.ru.domain.repository.ProjectsRepository
+import io.reactivex.Observable
+import javax.inject.Inject
+
+open class GetBookmarkedProjects @Inject constructor(
+    private val projectsRepository: ProjectsRepository,
+    postExecutionThread: PostExecutionThread
+) : ObservableUseCase<List<Project>, Nothing?>(postExecutionThread){
+
+    override fun buildUseCaseObservable(params: Nothing?): Observable<List<Project>> {
+        return projectsRepository.getBookmarkedProjects()
+    }
+
+}
