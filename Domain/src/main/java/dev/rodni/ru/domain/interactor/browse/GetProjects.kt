8f34379f159caa0f1f1002d78eedd2cc66deb4bd
rodniw <rodion.prosvirnin@bk.ru>
2019-09-09 18:05:25 +0300

    the first use case inside the domain layer written

---
+++ Domain/src/main/java/dev/rodni/ru/domain/interactor/browse/GetProjects.kt
@@ -0,0 +1,21 @@
+package dev.rodni.ru.domain.interactor.browse
+
+import dev.rodni.ru.domain.executor.PostExecutionThread
+import dev.rodni.ru.domain.interactor.ObservableUseCase
+import dev.rodni.ru.domain.model.Project
+import dev.rodni.ru.domain.repository.ProjectsRepository
+import io.reactivex.Observable
+import javax.inject.Inject
+
+//i expect list of projects back and i dont have any params
+//also im calling parents constructor and giving my post execution thread from my constructor to parent's constructor
+open class GetProjects @Inject constructor(
+    private val projectsRepository: ProjectsRepository,
+    postExecutionThread: PostExecutionThread
+) : ObservableUseCase<List<Project>, Nothing?>(postExecutionThread){
+
+    override fun buildUseCaseObservable(params: Nothing?): Observable<List<Project>> {
+        return projectsRepository.getProjects()
+    }
+
+}
