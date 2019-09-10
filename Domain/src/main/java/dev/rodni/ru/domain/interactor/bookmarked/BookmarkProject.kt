a4ccb5960cae8f911954faa1d64abd70148ae401
rodniw <rodion.prosvirnin@bk.ru>
2019-09-10 14:12:36 +0300

    BookmarkProjectTest created and passed for BookmarkProject use case. also decided to change "naming style" and use "Should" when i name some test.

--- Domain/src/main/java/dev/rodni/ru/domain/interactor/bookmarked/BookmarkProject.kt
+++ Domain/src/main/java/dev/rodni/ru/domain/interactor/bookmarked/BookmarkProject.kt
@@ -7,12 +7,12 @@ import io.reactivex.Completable
 import javax.inject.Inject
 
 //when a use tap to save a project
-open class BookmarkedProject @Inject constructor(
+open class BookmarkProject @Inject constructor(
     private val projectsRepository: ProjectsRepository,
     postExecutionThread: PostExecutionThread
-) : CompletableUseCase<BookmarkedProject.Params>(postExecutionThread) {
+) : CompletableUseCase<BookmarkProject.Params>(postExecutionThread) {
 
-    override fun buildUseCaseCompletable(params: Params?): Completable {
+    public override fun buildUseCaseCompletable(params: Params?): Completable {
         if (params == null) throw IllegalArgumentException("Params cant be null")
         return projectsRepository.bookmarkProject(params.projectId)
     }
---
+++ Domain/src/test/java/dev/rodni/ru/domain/interactor/bookmark/BookmarkProjectTest.kt
@@ -0,0 +1,49 @@
+package dev.rodni.ru.domain.interactor.bookmark
+
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.whenever
+import dev.rodni.ru.domain.executor.PostExecutionThread
+import dev.rodni.ru.domain.interactor.bookmarked.BookmarkProject
+import dev.rodni.ru.domain.repository.ProjectsRepository
+import dev.rodni.ru.domain.test.ProjectDataFactory
+import io.reactivex.Completable
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+
+//this test class is testing BookmarkProject use case
+class BookmarkProjectTest {
+
+    private lateinit var bookmarkProject: BookmarkProject
+    @Mock lateinit var projectsRepository: ProjectsRepository
+    @Mock lateinit var executionThread: PostExecutionThread
+
+    //setting up the constructor's dependencies
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+        bookmarkProject = BookmarkProject(projectsRepository, executionThread)
+    }
+
+    //needs parameters inside buildUseCase method
+    @Test
+    fun bookmarkProjectShouldCompletes() {
+        stubBookmarkProject(Completable.complete())
+        val testObserver = bookmarkProject.buildUseCaseCompletable(
+            BookmarkProject.Params.forProject(ProjectDataFactory.randomUuid())
+        ).test()
+        testObserver.assertComplete()
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun bookmarkProjectShouldThrowIllegalArgException() {
+        bookmarkProject.buildUseCaseCompletable().test()
+    }
+
+    //checking that calling to the repo returns an observable
+    private fun stubBookmarkProject(completable: Completable) {
+        whenever(projectsRepository.bookmarkProject(any()))
+            .thenReturn(completable)
+    }
+}
