27e242e8055d86ce330eb3f2a803e9b038c793a7
rodniw <rodion.prosvirnin@bk.ru>
2019-09-13 17:03:34 +0300

    before creating UI module.

---
+++ Presentation/src/test/java/dev/rodni/ru/presentation/viewmodel/BrowseBookmarkedProjectsViewModelTest.kt
@@ -0,0 +1,9 @@
+package dev.rodni.ru.presentation.viewmodel
+
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+//todo create tests for BrowseBookmarkedProjectsViewModel
+@RunWith(JUnit4::class)
+class BrowseBookmarkedProjectsViewModelTest {
+}
