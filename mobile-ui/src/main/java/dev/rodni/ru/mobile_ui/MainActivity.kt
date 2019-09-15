78222bb0251c0a091b89dda90fdc3f9dd9da6542
rodniw <rodion.prosvirnin@bk.ru>
2019-09-15 18:10:06 +0300

    set up main activity with nav controller.

--- mobile-ui/src/main/java/dev/rodni/ru/mobile_ui/MainActivity.kt
+++ mobile-ui/src/main/java/dev/rodni/ru/mobile_ui/MainActivity.kt
@@ -1,12 +1,44 @@
 package dev.rodni.ru.mobile_ui
 
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
+/**
+ * main app's activity
+ * which is a host for fragments
+ * with navigation framework
+ */
 class MainActivity: DaggerAppCompatActivity() {
+
+    /**
+     * nav controller instance for navigation
+     */
+    private lateinit var navController : NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        /**
+         * sets up nav controller
+         */
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        bottom_nav.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
     }
+
+    /**
+     * back button for fragments
+     */
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
+    }
+
 }
