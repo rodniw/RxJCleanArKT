887621ad9aeebcf4e37e0c20e1d6c2bc6957b2a2
rodniw <rodion.prosvirnin@bk.ru>
2019-09-15 15:19:21 +0300

    created application class.

--- mobile-ui/src/main/java/dev/rodni/ru/mobile_ui/MyApplication.kt
+++ mobile-ui/src/main/java/dev/rodni/ru/mobile_ui/MyApplication.kt
@@ -1,4 +1,22 @@
 package dev.rodni.ru.mobile_ui
 
-class MyApplication {
+import android.app.Application
+import timber.log.Timber
+
+/**
+ * main application class
+ */
+class MyApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        setupTimber()
+    }
+
+    /**
+     * sets up timber logs
+     */
+    private fun setupTimber() {
+        Timber.plant(Timber.DebugTree())
+    }
 }
